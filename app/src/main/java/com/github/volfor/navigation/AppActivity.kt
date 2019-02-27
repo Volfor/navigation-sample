c6c14aff0e55ff4032cab02f29da7875fcdb33d4
Volfor <valentine.trifonov@gmail.com>
2019-02-27 21:08:33 +0200

    added nav host fragment

--- app/src/main/java/com/github/volfor/navigation/AppActivity.kt
+++ app/src/main/java/com/github/volfor/navigation/AppActivity.kt
@@ -2,8 +2,11 @@ package com.github.volfor.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 
 class AppActivity : AppCompatActivity() {
+
+    val navController by lazy { findNavController(R.id.navHostFragment) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
